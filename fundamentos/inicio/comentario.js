a5cd921f9e2dd41ba3bffa1d7553d4e3800720a5
Wilber Lacerda de Jesus <wilberlacerda@hotmail.com>
2023-08-25 16:11:36 -0300

    new file:   fundamentos/inicio/bloco.js 	new file:   fundamentos/inicio/comentario.js 	new file:   fundamentos/inicio/dados_1.js 	new file:   fundamentos/inicio/dados_2.js 	new file:   fundamentos/inicio/dados_3.js 	new file:   fundamentos/inicio/dados_4.js 	new file:   fundamentos/inicio/desafio_area.js 	new file:   fundamentos/inicio/desafio_senteca_bloco.js 	new file:   fundamentos/inicio/desafio_troca.js 	new file:   fundamentos/inicio/sentenca_1.js 	new file:   fundamentos/inicio/sentenca_2.js 	new file:   fundamentos/operadores/aritimeticos.js 	new file:   fundamentos/operadores/desafio_aritimeticos.js 	new file:   fundamentos/operadores/relacionais_1.js 	new file:   fundamentos/operadores/relacionais_2.js

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,11 @@
+//console.log é usado para exibir algo na tela do computador!
+console.log("Passo #1")
+console.log("Passo #2") //mais um exemplo de console.log!
+console.log("Passo #3")
+
+/*
+    Esse é um
+    comentário
+    de múltiplas 
+    linhas
+*/
---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// PI * raio * raio
+
+const raio = 5
+const PI = 3.14
+let areaCirc = PI * raio * raio
+
+console.log("O valor da área da circunferência de raio: " + raio + " é: " + areaCirc + " m2.")
+
+areaCirc = Math.PI * raio * raio
+
+console.log("O valor da área da circunferência de raio: " + raio + " é: " + areaCirc + " m2.")
