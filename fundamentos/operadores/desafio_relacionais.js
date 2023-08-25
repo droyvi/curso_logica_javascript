5545b2d8b333b5b49559ef486d22c4efaa08eeba
Wilber Lacerda de Jesus <wilberlacerda@hotmail.com>
2023-08-25 16:49:35 -0300

    new file:   fundamentos/operadores/desafio_relacionais.js

---
+++ fundamentos/operadores/desafio_relacionais.js
@@ -0,0 +1,17 @@
+let a = 1
+let b = 2
+
+//encontre 5 expressões que o valor é TRUE
+console.log( a < b)
+console.log(a != b)
+console.log(a <= b)
+console.log(a !== b)
+console.log(a + a === b)
+
+//encontre 5 expressões que o valor é FALSE
+console.log(a > b)
+console.log(a == b)
+console.log(a >= b)
+console.log(a === b)
+console.log(a + a !== b)
+
