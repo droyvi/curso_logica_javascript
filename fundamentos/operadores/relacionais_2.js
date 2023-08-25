caecf1d0f0dd1d2df59c15c537831a5f299732f5
Wilber Lacerda de Jesus <wilberlacerda@hotmail.com>
2023-08-25 16:46:58 -0300

    fundamentos/operadores/desafio_relacionais.js

--- fundamentos/operadores/relacionais_2.js
+++ fundamentos/operadores/relacionais_2.js
@@ -1,17 +1,11 @@
-//valores literais
-console.log(7 > 9)
-console.log(7 >= 7)
-console.log(8 < 2)
-console.log(8 <= 12)
-console.log(9 == 3)
-console.log(9 != 5)
+let a = 1
+let b = "1"
+let c = "1"
 
-let a = 7
-let b = 32
-
-console.log(a > b)
-console.log(a >= b)
-console.log(a < b)
-console.log(a <= b)
 console.log(a == b)
+console.log(a === b)
+console.log(b === c)
+
 console.log(a != b)
+console.log(a !== b)
+console.log(b !== c)
