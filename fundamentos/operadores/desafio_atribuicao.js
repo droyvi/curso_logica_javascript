7e6b6d82ddb9799a1aae0c1ba88a934ab0912910
Wilber Lacerda de Jesus <wilberlacerda@hotmail.com>
2023-08-25 17:33:07 -0300

    new file:   fundamentos/operadores/atribuicao.js 	new file:   fundamentos/operadores/desafio_atribuicao.js

---
+++ fundamentos/operadores/desafio_atribuicao.js
@@ -0,0 +1,23 @@
+/*
+Lista de Aprovados
+==================
+
+1. Ana Silva
+2. Pedro Albuquerque
+3. Guilerme Pereira
+4. Rebeca França
+*/
+
+let texto = 'Lista de Aprovados'
+texto += '\n================='
+texto += '\n\n 1. Ana Silva'
+texto += '\n 2. Pedro Albuquerque'
+texto += '\n 1. Ana Silva'
+texto += '\n 3. Guilerme Pereira'
+texto += '\n 4. Rebeca França'
+
+console.log(texto)
+
+//let texto = 'Bom dia!'
+//texto += '\nTudo bem?'
+//console.log(texto)
