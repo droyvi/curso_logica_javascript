8bc8f2b8e01bdb5d4da15a89dddea882563a35d3
Wilber Lacerda de Jesus <wilberlacerda@hotmail.com>
2023-10-03 17:52:52 -0300

    new file:   fundamentos/controle/desafio_if.js 	new file:   fundamentos/controle/if_1.js 	new file:   fundamentos/controle/if_2.js 	new file:   fundamentos/controle/if_3.js 	new file:   fundamentos/controle/if_4.js

---
+++ fundamentos/controle/desafio_if.js
@@ -0,0 +1,23 @@
+const nota = 4
+
+if (nota <= 10 && nota >= 9) {
+    console.log('Conceito A!')
+}
+
+if (nota < 9 && nota >= 7) {
+    console.log('Conceito B!')
+}
+
+if (nota < 7 && nota >= 5) {
+    console.log('Conceito C!')
+}
+
+if (nota < 5 && nota >= 4.5) {
+    console.log('Conceito D!')
+}
+
+if (nota >= 0 && nota < 4.5) {
+    console.log('Conceito F!')
+}
+
+console.log('Fim!')
